73c34db63fec7deb5d1e347d1dbedcd0283bac69
Tinoproxz <jorgejimeneztenorio@gmail.com>
2023-09-15 21:05:19 -0300

    prueba1

---
+++ produ_APP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProduAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'produ_APP'
---
+++ produ_APP/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render (request,'webappp/index.html')
+
+def electronica(request):
+    data= {"titulo": "electronica"}
+    return render (request,'webappp/producto.html',data)
+
+def juguete(request):
+    data= {"titulo": "juguete"}
+    return render (request,'webappp/producto.html',data)
+
+def ropa(request):
+    data= {"titulo": "ropa1"}   
+    return render (request,'webappp/producto.html',data)
+
+    
